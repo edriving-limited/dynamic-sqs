900f5982df89740be9d2119164babbc60761749a
Keiron Lowe <keiron.lowe@edriving.com>
2023-04-28 10:49:59 +0100

    Fixed test, removed db

---
+++ tests/Unit/CustomTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace eDriving\CustomSqsDriver\Tests\Unit;
+
+use eDriving\CustomSqsDriver\Tests\TestCase;
+
+class CustomTest extends TestCase
+{
+    public function test_something(): void
+    {
+        $this->assertTrue(true);
+    }
+}
