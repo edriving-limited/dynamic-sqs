36f822c5c1d209b31ae0644d918147dab21a2a71
Keiron Lowe <keiron.lowe@edriving.com>
2023-04-28 11:46:08 +0100

    Switched to get method to fix phpstan error

--- src/Providers/CustomSqsDriverServiceProvider.php
+++ src/Providers/CustomSqsDriverServiceProvider.php
@@ -9,7 +9,7 @@ class CustomSqsDriverServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $manager = $this->app['queue'];
+        $manager = $this->app->get('queue');
 
         $manager->addConnector('custom-sqs', function () {
             return new CustomSqsConnector();
