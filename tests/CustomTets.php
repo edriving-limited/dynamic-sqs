5025b613aa25872ea74fe2a6e31584d7fcc550eb
Keiron Lowe <keiron.lowe@edriving.com>
2023-04-28 10:47:29 +0100

    added dummy test

---
+++ tests/CustomTets.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace eDriving\CustomSqsDriver\Tests;
+
+class CustomTets extends TestCase
+{
+    public function test_something(): void
+    {
+        $this->assertTrue(true);
+    }
+}
