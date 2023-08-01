548697bb2e5115a899d1743df65b099ed8194dba
Keiron Lowe <keiron.lowe@edriving.com>
2023-08-01 15:55:35 +0100

    Updated allowed types

--- src/Contracts/JobHandlerContract.php
+++ src/Contracts/JobHandlerContract.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 interface JobHandlerContract
 {
     /**
-     * @param array<string, mixed> $payload
+     * @param array<string, string | number | null | bool | array<mixed>> $payload
      * @return ShouldQueue
      */
     public function handle(array $payload): ShouldQueue;
